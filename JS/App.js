adc995b80e39fc5969133560aaefd09093979388
Polo <mphowitzer@gmail.com>
2022-09-18 23:36:02 -0500

    aEntregar

--- JS/App.js
+++ JS/App.js
@@ -1,12 +1,11 @@
-const item = document.getElementById("item");
-
+const card = document.getElementById("card");
 
 
 const disfraces = [
     {
         id: 1,
         nombre: "Vegeta",
-        precio: 350,
+        precio: 350.00,
         img: "img/Sayayin.jpg",
         descripcion: "Disfraz de Sayayin modelo Vegeta.",
         tallas: "Tallas CH - G",
@@ -14,24 +13,24 @@ const disfraces = [
     {
         id: 2,
         nombre: "Princes",
-        precio: 450,
-        img: "../img/princes.jpg",
+        precio: 450.00,
+        img:"img/princes.jpg",
         descripcion: "Disfraz de Princesa en color rosa.",
         tallas: "Tallas CH - XL",
     },
     {
         id: 3,
         nombre: "Sirenita",
-        precio: 420,
-        img: "../img/Ariel.jpg",
+        precio: 420.00,
+        img: "img/Ariel.jpg",
         descripcion: "Disfraz de Sirenita, modelo de la película original.",
         tallas: "Tallas CH - G",
     },
     {
         id: 4,
         nombre: "Hipo",
-        precio: 390,
-        img: "./img/ipo.jpg",
+        precio: 390.00,
+        img: "img/ipo.jpg",
         descripcion: 'Disfraz de Hipo "Como entrenar a tu dragón"',
         tallas: "Tallas CH - XXL",
     },
@@ -40,13 +39,16 @@ const disfraces = [
 let carrito = [];
 
 disfraces.forEach((disfraz) => {
-    let content = document.createElement("div");
-    content.innerHTML = `
-        <img src = ${disfraz.img}">
-        <p>${disfraz.descripcion}</p>
-        <p>${disfraz.tallas}</p>
-        <label>${disfraz.precio}</label>
-    `;
+    let informacion = document.createElement("div");
+    informacion.className = "formStyle"
+    informacion.innerHTML = `
+    <img class="styleImgFlt" src = ${disfraz.img}>
+    <p class="style">${disfraz.descripcion}</p>
+    <p class="style">${disfraz.tallas}</p>
+    <label><b>Precio: $ ${disfraz.precio}</b></label><br>
+    <button class="boton" type="submit"><span>Agregar</span></button>
+`;
 
-    item.append(content);
+card.append(informacion);
 });
+
